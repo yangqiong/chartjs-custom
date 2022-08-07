802f2925d07347d9a0a583c66a57dc3964988c09
yangqiong <yangqionggo@163.com>
2022-08-07 13:23:47 +0800

    init

---
+++ src/Utils.ts
@@ -0,0 +1,113 @@
+import { valueOrDefault } from 'chart.js/helpers';
+import colorLib from '@kurkle/color';
+
+let _seed = Date.now();
+
+const CHART_COLORS = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)'
+};
+
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+function months(config: { count?: number, section?: number }) {
+  var cfg = config || {};
+  var count = cfg.count || 12;
+  var section = cfg.section;
+  var values = [];
+  var i, value;
+
+  for (i = 0; i < count; ++i) {
+    value = MONTHS[Math.ceil(i) % 12];
+    values.push(value.substring(0, section));
+  }
+
+  return values;
+}
+
+function rand(min?: number, max?: number) {
+  min = valueOrDefault(min, 0);
+  max = valueOrDefault(max, 0);
+  _seed = (_seed * 9301 + 49297) % 233280;
+  return min + (_seed / 233280) * (max - min);
+}
+
+interface Config {
+  min?: number,
+  max?: number,
+  from?: number[],
+  count?: number,
+  decimals?: number,
+  continuity?: number,
+  r?: number,
+  rmin?: number,
+  rmax?: number
+}
+
+function numbers(config: Config) {
+  var cfg = config || {};
+  var min = valueOrDefault(cfg.min, 0);
+  var max = valueOrDefault(cfg.max, 100);
+  var from = valueOrDefault(cfg.from, []);
+  var count = valueOrDefault(cfg.count, 8);
+  var decimals = valueOrDefault(cfg.decimals, 8);
+  var continuity = valueOrDefault(cfg.continuity, 1);
+  var dfactor = Math.pow(10, decimals) || 0;
+  var data = [];
+  var i, value;
+
+  for (i = 0; i < count; ++i) {
+    value = (from[i] || 0) + rand(min, max);
+    if (rand() <= continuity) {
+      data.push(Math.round(dfactor * value) / dfactor);
+    } else {
+      data.push(null);
+    }
+  }
+
+  return data;
+}
+
+function transparentize(value: string, opacity: number) {
+  var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+  return colorLib(value).alpha(alpha).rgbString();
+}
+
+function points(config: Config) {
+  const xs = numbers(config);
+  const ys = numbers(config);
+  return xs.map((x, i) => ({ x, y: ys[i], r: 5 }));
+}
+
+function bubbles(config: Config) {
+  return points(config).map(pt => {
+    pt.r = rand(config.rmin, config.rmax);
+    return pt;
+  });
+}
+
+export default {
+  CHART_COLORS,
+  months,
+  numbers,
+  transparentize,
+  bubbles
+}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import Utils from './Utils';
+import { Chart, ChartConfiguration, BubbleDataPoint, ChartData, LineElement, BubbleController, CategoryScale, LinearScale, PointElement } from 'chart.js'
+Chart.register(BubbleController, CategoryScale, LinearScale, PointElement, LineElement)
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = { count: DATA_COUNT, rmin: 5, rmax: 15, min: 0, max: 100 }
+
+const points = Utils.bubbles(NUMBER_CFG) as BubbleDataPoint[];
+const data: ChartData = {
+  labels: Utils.months({ count: 7 }),
+  datasets: [{
+    type: 'bubble',
+    data: points.map(point => { point.r = 10; return point; }),
+    backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+  }]
+};
+
+const config: ChartConfiguration = {
+  type: 'bubble',
+  data
+};
+
+const $myChart = document.getElementById('myChart') as HTMLCanvasElement;
+if ($myChart) {
+  new Chart($myChart, config);
+}
