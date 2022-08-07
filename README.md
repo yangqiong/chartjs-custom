1a21806d18fb0185a6e1876c635e61d8ff66ee9c
yangqiong <yangqionggo@163.com>
2022-08-07 14:45:21 +0800

    feat: Bubble | Selected Point and Show Horizol and Vertical Line

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Chart.js Custom
+
+## Bubble | Selected Point and Show Horizol and Vertical Line
+
+- remove xAxis grid and remove yAxis grid and border
+- hover on the point change cursur to pointer
+- select an point and draw a border around the point
+- select an point and draw the horizol and vertical line
+
+![bubble-selected](./docs/images/bubble-selected.png)
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ const data: ChartData = {
     type: 'bubble',
     data: points.map(point => { point.r = 10; return point; }),
     backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+    hoverRadius: 0,
   }]
 };
 
@@ -37,13 +38,68 @@ const options = {
     if (chartElements.length == 0) {
       target.style.cursor = "default";
     }
+  },
+  onClick(_: ChartEvent, chartElements: ActiveElement[], chart: Chart) {
+    if (chartElements.length == 1) {
+      chart.config.options.plugins["selected"] = {
+        datasetIndex: chartElements[0].datasetIndex,
+        index: chartElements[0].index,
+      }
+      chart.update();
+    }
+  },
+  plugins: {
+    "selected": {
+      datasetIndex: 0,
+      index: 0,
+    }
+  }
+}
+
+const plugin = {
+  id: "selected",
+  afterDraw: (chart: Chart, _, options: options) => {
+    if (options.datasetIndex >= 0 && options.index >= 0) {
+      chart.data.datasets.forEach(function (_, i) {
+        const meta = chart.getDatasetMeta(i);
+        if (i === options.datasetIndex) {
+          const data = meta.data[options.index];
+          const { x, y } = data;
+          const radius = data.options.radius + 1;
+
+          const ctx = chart.ctx;
+          const { chartArea } = chart;
+
+          ctx.save();
+          ctx.beginPath();
+          ctx.arc(x, y, radius, 0, 2 * Math.PI);
+          ctx.strokeStyle = "#7DDDB5";
+          ctx.lineWidth = 2;
+          ctx.stroke();
+          ctx.beginPath();
+          ctx.setLineDash([5, 2]);
+          ctx.moveTo(x, y + radius + 2);
+          ctx.lineTo(x, chartArea.bottom);
+          ctx.stroke();
+
+          ctx.beginPath();
+          ctx.setLineDash([5, 2]);
+          ctx.moveTo(chartArea.left, y);
+          ctx.lineTo(x - radius - 2, y);
+          ctx.stroke();
+
+          ctx.restore();
+        }
+      });
+    }
   }
 }
 
 const config: ChartConfiguration = {
   type: 'bubble',
   data,
-  options
+  options,
+  plugins: [plugin]
 };
 
 const $myChart = document.getElementById('myChart') as HTMLCanvasElement;
